bf2a000ed08a740d6a11726d5fca46afb90a3d1f
mohcine <mohcine.sakhi@gmail.com>
2022-06-17 12:09:28 +0100

    Add Solution 14 of hackerRank

--- src/hackerRank/Solution04.java
+++ src/hackerRank/Solution04.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Solution04 {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		String A = sc.next();
-		String B = sc.next();
+		Scanner scanner = new Scanner(System.in);
+		String A = scanner.next();
+		String B = scanner.next();
 		/* Enter your code here. Print output to STDOUT. */
 
 		System.out.println(A.length() + B.length());
@@ -20,7 +20,7 @@ public class Solution04 {
 		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " "
 				+ B.substring(0, 1).toUpperCase() + B.substring(1));
 
-		sc.close();
+		scanner.close();
 
 	}
 }
